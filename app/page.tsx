f6b7a9c16f4d23e813d1d4d2bebf87f5bc1e956b
qurrahazam <qurrahazam@gmail.com>
2026-03-02 12:32:52 +0500

    UPDATE: included services, stas, why coose us,  contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import ServicesSection from "@/components/ServiceSession";
+import StatsSection from "@/components/StatsSection";
+import WhyChooseUs from "@/components/whyChooseus";
+import ContactSection from "@/components/Contactus";
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <ServicesSection />
+      <StatsSection />
+      <WhyChooseUs />
+      <ContactSection />
+    </div>
   );
 }
