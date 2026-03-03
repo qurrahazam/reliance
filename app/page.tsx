1af21b364bf7d7003189c0c27ebdd3d71cad74da
qurrahazam <qurrahazam@gmail.com>
2026-03-03 15:41:11 +0500

    UPDATE: hero and footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import ServicesSection from "@/components/ServiceSession";
 import StatsSection from "@/components/StatsSection";
 import WhyChooseUs from "@/components/whyChooseus";
 import ContactSection from "@/components/Contactus";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <div>
+      <Hero />
       <ServicesSection />
       <StatsSection />
       <WhyChooseUs />
       <ContactSection />
+      <Footer />
     </div>
   );
 }
